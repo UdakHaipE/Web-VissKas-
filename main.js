5fefe57b85dd739c2280292867bef3c965b371ad
Ulvis Ozolins <47106597+UdakHaipE@users.noreply.github.com>
2022-12-21 15:09:43 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    // paņem "NavBar" elementus
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  
+    // Padara katru nospiežamu
+    $navbarBurgers.forEach( el => {
+      el.addEventListener('click', () => {
+  
+        // Dabū datus ko vajag izvadīt
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+  
+        // aktivizē "navBar" un "navbar-burger"
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+  
+      });
+    });
+  
+  });
